4233a2e4cec64e3d210d648f7db5da320ba94288
Rémi Labergère <remi.labergere@gmail.com>
2020-08-20 13:37:45 +0200

    Project setup
    Signed-off-by: Rémi Labergère <remi.labergere@gmail.com>

---
+++ src/main/java/fr/rems19/betterbonemeal/BetterBonemeal.java
@@ -0,0 +1,10 @@
+package fr.rems19.betterbonemeal;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterBonemeal implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
